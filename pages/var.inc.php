bad4dd251ab2d324a737e3fce42b0ffd1b940ad5
AndyL94 <andy.louis94@outlook.fr>
2022-11-04 10:44:56 +0100

    PHP Update

--- pages/var.inc.php
+++ pages/var.inc.php
@@ -1,5 +1,5 @@
 <?php
-     $_image = '<img src="https://cdn.discordapp.com/attachments/1023894255716028426/1025033246192455761/4711081597780_h_f_l_0.webp" alt="">';
+     $_image = '<img src="https://cdn.discordapp.com/attachments/1023894255716028426/1025033246192455761/4711081597780_h_f_l_0.webp" alt="" loading="lazy">';
      $_processeur = '<strong>Processeur</strong> : AMD Ryzen 7 4800H';
      $_carte_graphique = '<strong>Carte Graphique</strong> : NVIDIA GeForce RTX 3050Ti';
      $_ecran = '<strong>Ecran</strong> : "17.3"(43.9 cm) - Mémoire vive (RAM) : 16 Go';
